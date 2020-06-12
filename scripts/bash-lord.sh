1923933dc75ba0d0e3ec585218dbc09651f6bfbf
Sairaj Madhavan <sairaj.madhavan@keap.com>
2020-06-11 22:58:46 -0700

    Ability to set a project, also logic broken into functions

--- scripts/bash-lord.sh
+++ scripts/bash-lord.sh
@@ -1,41 +1,25 @@
-option1=${1?Error: No options provided. For a complete list of options type bash-lord help}
-
-if [ -z "${1}" ];
-then
-    echo ${option1}
-    exit
-fi
-
-if [ "${option1}" =  "help" ];
-then
-    cat help.txt
-    exit
-elif [ "${option1}" = "g" ];
-then
-    option2=${2?Error: No secondary option provided. Try bash-lord help}
-    if [ -z "${2}" ];
+#!/bin/bash
+function check_empty() {
+    if [ -z "${1}" ];
     then
-        echo ${option2}
+        echo ${2}
         exit
     fi
+}
+
+function gcloud_ops() {
+    option2=${2?Error: No secondary option provided. Try bash-lord help}
+    check_empty $2 ${option2}
 
     if [ "${option2}" = "projects" ];
     then
         option3=${3?Error: Can be get or set}
-        if [ -z "${3}" ];
-        then
-            echo ${option3}
-            exit
-        fi
+        check_empty $3 ${option3}
 
         if [ "${option3}" = "get" ];
         then
             option4=${4?Error: Can be all or partial name or complete name}
-            if [ -z "${4}" ];
-            then
-                echo ${option4}
-                exit
-            fi
+            check_empty $4 ${option4}
 
             if [ "${option4}" = "all" ];
             then
@@ -43,11 +27,32 @@ then
             else
                 gcloud projects list --filter=${option4}
             fi
+        elif [ "${option3}" = "set" ];
+        then
+            option4=${4?Error: Should be complete name of the gcloud project. Use get all to get project names first.}
+            check_empty $4 ${option4}
+            gcloud config set project ${option4}
+        else
+            echo "Error: Can be get or set"
+            exit
         fi
     else
         echo "Error: Not a recognized operation. Try projects for gcloud; contexts for k8s"
         exit
     fi
+}
+
+option1=${1?Error: No options provided. For a complete list of options type bash-lord help}
+
+check_empty $1 ${option1}
+
+if [ "${option1}" =  "help" ];
+then
+    cat help.txt
+    exit
+elif [ "${option1}" = "g" ];
+then
+    gcloud_ops $1 $2 $3 $4
 else
     echo "Error: Not a recognized operation. Try g or k. g for gcloud; k for k8s"
 fi
